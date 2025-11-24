e840f9e9634cf2c6801352ebf95d9f0ecfd5c227
slirez <e.lugo24@info.uas.edu.mx>
2025-11-23 21:01:18 -0700

    init: proyecto Cantantes React (Vite + Router + Bootstrap)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,28 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom'
 
-export default function NavBar() {
+export default function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="brand">Cantantes</div>
-      <ul className="nav-links">
-        <li><NavLink to="/" end className={({isActive}) => isActive ? 'active' : ''}>Inicio</NavLink></li>
-        <li><NavLink to="/gallery" className={({isActive}) => isActive ? 'active' : ''}>Galería</NavLink></li>
-        <li><NavLink to="/about" className={({isActive}) => isActive ? 'active' : ''}>Acerca</NavLink></li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container">
+        <Link className="navbar-brand fw-bold" to="/">Cantantes</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="mainNav">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink end className="nav-link" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/singers">Lista de Cantantes</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">About</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-  );
+  )
 }
